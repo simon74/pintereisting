4fe5fb5315e32496b807209c71ee79d4cbf04be1
simon74 <simon@hemph.se>
2016-08-19 22:04:05 +0200

    New User Signup and Signin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Pintereisting::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #devise gem for heroku
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pintereisting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # note to set this to domain name.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
